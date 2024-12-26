0998335494de7dbf5efa89136a7105c6d1f9f129
Voinea Radu <contact@voinearadu.com>
2024-12-26 22:18:36 +0200

    Small changes to font handling and sizing on mobile

--- html/assets/js/index.js
+++ html/assets/js/index.js
@@ -1,6 +1,10 @@
 registerExperienceButtons(8);
 registerExperienceHover(8);
 
+function get_handle_size(){
+    return 38;
+}
+
 function getElement(_class){
     return document.getElementsByClassName(_class)[0];
 }
@@ -10,8 +14,6 @@ function getElements(_class){
 }
 
 function registerExperienceButtons(amount) {
-    let size = 38.5;
-
     for (let i = 1; i <= amount; i++) {
         let buttonClass = "experience-select-" + i;
         let experineceClass = "experience-" + i;
@@ -25,13 +27,13 @@ function registerExperienceButtons(amount) {
             let activeSelectParent = getElement("experience-select-active-parent");
             let bar = getElement("experience-bar-handle");
 
-            bar.style.top = size * (i - 1) + "px";
+            bar.style.top = get_handle_size() * (i - 1) + "px";
             activeSelect.classList.remove("experience-select-active");
             activeSelectParent.classList.remove("experience-select-active-parent");
             button.classList.add("experience-select-active");
             button.parentNode.classList.add("experience-select-active-parent");
 
-            if (active != undefined) {
+            if (active !== undefined) {
                 const list = [...active];
                 for (let index = 0; index < list.length; index++) {
                     const element = list[index];
@@ -39,7 +41,7 @@ function registerExperienceButtons(amount) {
                     element.classList.remove("experience-active");
                 }
             }
-            if (experience != undefined) {
+            if (experience !== undefined) {
                 const list = [...experience];
                 for (let index = 0; index < list.length; index++) {
                     const element = list[index];
@@ -52,9 +54,7 @@ function registerExperienceButtons(amount) {
 }
 
 function registerExperienceHover(amount) {
-    let size = 38.5;
-    
-    getElement("experience-bar").style.height = size * amount + "px";
+    getElement("experience-bar").style.height = get_handle_size() * amount + "px";
 
     for (let i = 1; i <= amount; i++) {
         let buttonClass = "experience-select-" + i;
@@ -63,7 +63,7 @@ function registerExperienceHover(amount) {
         let bar = getElement("experience-bar-handle");
 
         button.parentNode.addEventListener("mouseover", () => {
-            bar.style.top = size * (i - 1) + "px";
+            bar.style.top = get_handle_size() * (i - 1) + "px";
         });
         button.parentNode.addEventListener("mouseout", () => {
             let active = getElement("experience-select-active");
@@ -71,7 +71,7 @@ function registerExperienceHover(amount) {
             let numberString = activeClass.replace("experience-select-", "");
             let number = parseInt(numberString);
 
-            bar.style.top = size * (number - 1) + "px";
+            bar.style.top = get_handle_size() * (number - 1) + "px";
         });
     }
 }
